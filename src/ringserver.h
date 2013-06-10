c65f665b031481dc7ed80e82e229f6409f3c0a87
Chad Trabant <chad@iris.washington.edu>
2013-06-10 17:56:38 +0000

    update version number

--- src/ringserver.h
+++ src/ringserver.h
@@ -15,7 +15,7 @@ extern "C" {
 #endif
 
 #define PACKAGE   "ringserver"
-#define VERSION   "2012.317"
+#define VERSION   "2013.161"
 
 /* Thread data flags */ 
 #define TDF_SPAWNING    (1<<0)          /* Thread is now spawning   */
