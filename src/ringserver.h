856731ddef26f6cd3d568813e0c205b0a8831998
chad-iris <chad.trabant@iris.edu>
2018-02-14 00:06:53 -0800

    Bump version

--- src/ringserver.h
+++ src/ringserver.h
@@ -15,7 +15,7 @@ extern "C" {
 #endif
 
 #define PACKAGE   "ringserver"
-#define VERSION   "2017.052"
+#define VERSION   "2018.044"
 
 /* Thread data flags */
 #define TDF_SPAWNING    (1<<0)          /* Thread is now spawning   */
