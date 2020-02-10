0265fdf699e9d310508b8b54fd20df6bd374c7cc
chad-iris <chad.trabant@iris.edu>
2020-02-09 18:58:15 -0800

    Update license for ringserver code to Apache 2

--- src/dsarchive.h
+++ src/dsarchive.h
@@ -3,10 +3,22 @@
  *
  * Routines to archive miniSEED data records, specialized for ringserver.
  *
- * Written by Chad Trabant
- *   IRIS Data Management Center
+ * This file is part of the ringserver.
  *
- * modified: 2010.020
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2020:
+ * @author Chad Trabant, IRIS Data Management Center
  ***************************************************************************/
 
 #ifndef DSARCHIVE_H
